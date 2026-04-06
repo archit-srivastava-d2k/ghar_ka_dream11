7dfe2dfbc3bbd786e774448927f4b19f1d4760a0
Archit-D2K <i-archit.rivastava@d2kindia.com>
2026-04-06 13:07:50 +0530

    fix: let Vercel handle NEXT_PUBLIC env var natively

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:4000',
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
